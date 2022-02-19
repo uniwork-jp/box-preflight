55ff6aaecf617fd8461d0d572e4c7276d1f72347
ikkikki <50198675+ikkikki@users.noreply.github.com>
2022-02-20 00:20:07 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,24 +2,27 @@
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
+
+ "gatsby-plugin-sitemap",
+ {
+   resolve: "gatsby-plugin-canonical-urls",
+   options: {
+     siteUrl: "https:",
+   },
+ },
+
+
  */
 
 module.exports = {
   siteMetadata: {
-    title: "Uniwork.presents",
+    title: "Uniwork.presents"
   },
   plugins: [
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-canonical-urls",
-      options: {
-        siteUrl: "https:",
-      },
-    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-  ],
+    "gatsby-transformer-sharp"
+  ]
 };
